7146d3827aa4ad840d22b8d0eda6cfaf297b40ab
Alina Frenchel <alina.frenchel@gmail.com>
2023-03-09 20:09:33 +0200

    project setup env, start navbar

---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,19 @@
+import testimonials from './testimonials'
+import work from './work'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+export const schemaTypes = [
+  testimonials,
+  work,
+  brands,
+  abouts,
+  experiences,
+  skills,
+  workExperience,
+  contact,
+]
