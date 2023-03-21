3f7600b4c2aa646185c65e767391fdd0d9e0b685
Alina Frenchel <alina.frenchel@gmail.com>
2023-03-21 16:22:55 +0200

    small changes-personalize

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,17 +1,26 @@
 import React from "react";
-import { BsTwitter, BsInstagram } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
-
+import { AiFillGithub } from "react-icons/ai";
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <a href="https://www.facebook.com/roman.alinaioana" target="_blank">
+        <FaFacebookF />
+      </a>
     </div>
     <div>
-      <FaFacebookF />
+      <a
+        href="https://www.linkedin.com/in/alina-frenchel-a9337b22a/"
+        target="_blank"
+      >
+        <BsLinkedin />
+      </a>
     </div>
     <div>
-      <BsInstagram />
+      <a href="https://github.com/afrenchel" target="_blank">
+        <AiFillGithub />
+      </a>
     </div>
   </div>
 );
